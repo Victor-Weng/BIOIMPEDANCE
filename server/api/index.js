e12d06b3de5d298c979141aa881c34263e40d268
Victor-Weng <VictorWengVTW@gmail.com>
2024-07-12 20:07:31 +0800

    vercel configuration

---
+++ server/api/index.js
@@ -0,0 +1,48 @@
+const express = require("express"); // Get the library
+const app = express(); // make the app run express
+const cors = require("cors"); // lets front end make requests to back end
+const pool = require("./db"); // require the db.js file from parent folder
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//ROUTES//
+
+// get all bios
+
+app.get("/bios", async(req, res) => {
+    try {
+        const allBios = await pool.query("SELECT * FROM bio");
+        res.json(allBios.rows);
+    } catch (err) {
+        console.error(err.message);
+        
+    }
+});
+
+// get a specific bio
+
+app.get("/bios/:location/:size/:depth/:frequency", async (req, res) => { 
+    try {
+        const { location, size, depth, frequency } = req.params;
+        const bio = await pool.query(
+            "SELECT * FROM bio WHERE location = $1 AND size = $2 AND depth = $3 AND frequency = $4", 
+            [location, size, depth, frequency]
+        );
+        
+        res.json(bio.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Error in index.js" });
+    }
+});
+
+
+//port = 5000; // express port
+
+app.listen(() => {
+    console.log("Server has started.");
+});
+
+module.exports = app;
